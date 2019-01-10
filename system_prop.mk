1c7a0520547e0e70f9dd052c38c4dc8358ccc793
Anushek Prasal <anushekprasal@gmail.com>
2019-01-11 02:39:07 +0530

    universal8895: Force HAL1 for certain apps
    Change-Id: Ib86311cb638dacfc9d21aca6d0412471d1c260e1

--- system_prop.mk
+++ system_prop.mk
@@ -78,7 +78,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=true \
     ro.config.vc_call_vol_steps=5 \
     debug.sf.latch_unsignaled=1 \
-    debug.stagefright.ccodec=0
+    debug.stagefright.ccodec=0 \
+    vendor.camera.hal1.packagelist=com.whatsapp,com.facebook.katana,com.instagram.android,com.snapchat.android
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
