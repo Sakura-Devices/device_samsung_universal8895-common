laki9/android_device_samsung_universal8895-common

universal8895: Add libstagefright shim for NV21 color format
Signed-off-by: Albe96XDA <albe96xda@gmail.com>

BoardConfigCommon.mk
@@ -238,4 +238,6 @@ TARGET_LD_SHIM_LIBS += \
     /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
     /system/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so \
     /system/lib64/hw/fingerprint.vendor.exynos5.so|/vendor/lib64/libbauthtzcommon_shim.so \
-    /system/bin/gpsd|/vendor/lib64/gpsd_shim.so
+    /system/bin/gpsd|/vendor/lib64/gpsd_shim.so \
+    /system/lib64/libstagefright.so|/vendor/lib64/libstagefright_shim.so \
+    /system/lib/libstagefright.so|/vendor/lib/libstagefright_shim.so

Sakura-Devices/device_samsung_universal8895-common

Change-Id: Id357814fc119f8e86f16a0e9b6559cc0de2b4d8b

BoardConfigCommon.mk
@@ -146,7 +146,9 @@ BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 # Shims
 TARGET_LD_SHIM_LIBS += \
     /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so \
-    /system/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so
+    /system/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so \
+    /system/lib/libstagefright.so|/vendor/lib/libstagefright_shim.so \
+    /system/lib64/libstagefright.so|/vendor/lib64/libstagefright_shim.so
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)